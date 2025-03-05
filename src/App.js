4c47d72ff0d40f83b1a4244531f08e2ce1a614d4
Jakdeveloper1 <Jakdeveloper@gmail.com>
2025-03-05 07:05:06 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,69 +1,97 @@
-// import logo from './logo.svg';
-import './App.css';
-import React, { useEffect, useState } from "react"
-import  {Second}  from './Components/Seconds';
-import App from './Components/CounterApp';
-import { ToDoList } from './Components/ToDoList';
-import { Fetch } from './Components/fetchData';
-import { App1 } from './Components/FormData';
-import {  Link, Outlet } from 'react-router-dom';
+// // import logo from './logo.svg';
+// import './App.css';
+// import React, { useEffect, useState } from "react"
+// import  {Second}  from './Components/Seconds';
+// import App from './Components/CounterApp';
+// import { ToDoList } from './Components/ToDoList';
+// import { Fetch } from './Components/fetchData';
+// import { App1 } from './Components/FormData';
+// import {  Link, Outlet } from 'react-router-dom';
 
-function First () {
+// function First () {
 
-  var name = "Shehryar"
-  const second = <Second/>
+//   var name = "Shehryar"
+//   const second = <Second/>
 
-  const [getName, setName] = useState("")
+//   const [getName, setName] = useState("")
 
   
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+        
+        
+//         {/* <img src={logo} className="App-logo"  /> */}
+//         <p>
+//           Edit <code>src/App.js name =  {name} </code> and save to reload.
+//         </p>
+//         {/* <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a> */}
+//       </header>
+//           {/* {second} */}
+//       {/* <Second name="First" />
+//       <Second name="Second"/>
+//       <Second name="Third"/>
+// */}
+
+//       {/* <App/>  */}
+
+//         {/* <Test/> */}
+
+//       {/* <Fetch/>
+
+//       <App1 title = "Name" state ={getName}></App1>
+//       <App1 title = "Roll No"></App1>
+//       <App1 title = "Age"></App1>
+//       <button onClick={{}}>Submit</button> */}
+
+//       {/* <ToDoList/> */}
+//       <Link to="/contact/:id">Contact component </Link>
+//       <br/>
+//       <Link to="/test">Load my Test links component</Link>
+//       <Outlet/>
+      
+
+//     </div>
+    
+//   );
+
+// }
+
+
+
+// export default First;
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        
-        {/* <img src={logo} className="App-logo"  /> */}
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js name =  {name} </code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
+          GitHub Actions
         </p>
-        {/* <a
+        <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a> */}
+        </a>
+
+        
       </header>
-          {/* {second} */}
-      {/* <Second name="First" />
-      <Second name="Second"/>
-      <Second name="Third"/>
-*/}
-
-      {/* <App/>  */}
-
-        {/* <Test/> */}
-
-      {/* <Fetch/>
-
-      <App1 title = "Name" state ={getName}></App1>
-      <App1 title = "Roll No"></App1>
-      <App1 title = "Age"></App1>
-      <button onClick={{}}>Submit</button> */}
-
-      {/* <ToDoList/> */}
-      <Link to="/contact/:id">Contact component </Link>
-      <br/>
-      <Link to="/test">Load my Test links component</Link>
-      <Outlet/>
-      
-
     </div>
-    
   );
-
 }
 
-
-
-export default First;
+export default App;
